d1b4f0bb88814d73dc977fae6ee305b685026f80
SaadinoZ1 <shicham230@gmail.com>
2024-03-21 13:47:07 +0000

    First Commit

---
+++ LoginApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using LoginApi.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginApi.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> User { get; set; }
+
+    }
+}
